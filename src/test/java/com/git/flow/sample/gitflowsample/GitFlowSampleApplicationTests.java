2c9090a7e8535911692224bbe4741fc10bad7188
Rock Jiang <limengcanyu@163.com>
2020-09-03 10:05:22 +0800

    init

---
+++ src/test/java/com/git/flow/sample/gitflowsample/GitFlowSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.git.flow.sample.gitflowsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitFlowSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
